e946a58b70071e02ea25de381eb4fe3c7a720be7
theadammason <adam.mason86@gmail.com>
2017-03-20 13:09:13 -0400

    object attribute

--- public/scripts/trophyController.js
+++ public/scripts/trophyController.js
@@ -6,100 +6,99 @@ app.controller('trophyController', function ($scope, studentFactory) {
 
     //array to hold trophy image objects
     $scope.trophies = [{
-        sauce: '../img/trophyImages/oneSmallStepTrophy.png',
+        source: '../img/trophyImages/oneSmallStepTrophy.png',
         alternate: 'one small step',
         trophyName: 'one small step',
         locked: false
-        
+
     }, {
-        sauce: '../img/trophyImages/trophy.png',
+        source: '../img/trophyImages/trophy.png',
         alternate: 'read more than 50 pages',
         trophyName: 'read more than 50 pages',
         locked: false
-    }, 
+    },
        {
-        sauce: '../img/trophyImages/tripleThreatTrophy.png',
+        source: '../img/trophyImages/tripleThreatTrophy.png',
         alternate: 'triple threat!',
         trophyName: 'triple threat!',
         locked: false
-    },                  
-                       
+    },
+
                        {
-        sauce: '../img/trophyImages/fabFiveTrophy.png',
+        source: '../img/trophyImages/fabFiveTrophy.png',
         alternate: 'fab five!',
         trophyName: 'fab five!',
         locked: false
     }, {
-        sauce: '../img/trophyImages/doubleDigitalTrophy.png',
+        source: '../img/trophyImages/doubleDigitalTrophy.png',
         alternate: 'double digital!',
         trophyName: 'double digital!',
         locked: false
     }, {
-        sauce: '../img/trophyImages/trophy.png',
+        source: '../img/trophyImages/trophy.png',
         alternate: 'read for longer than 30 minutes',
         trophyName: 'read for longer than 30 minutes',
         locked: false
-        
+
     }, {
-        sauce: '../img/trophyImages/walkingLibraryTrophy.png',
+        source: '../img/trophyImages/walkingLibraryTrophy.png',
         alternate: 'walking library!',
         trophyName: 'walking library!',
         locked: false
     }, {
-        sauce: '../img/trophyImages/25Trophy.png',
+        source: '../img/trophyImages/25Trophy.png',
         alternate: 'crucial quarter!',
         trophyName: 'crucial quarter!',
         locked: false
     },
         {
-        sauce: '../img/trophyImages/trophy.png',
+        source: '../img/trophyImages/trophy.png',
         alternate: 'over 100 pages!',
         trophyName: 'over 100 pages!',
         locked: false
     },
             {
-        sauce: '../img/trophyImages/trophy.png',
+        source: '../img/trophyImages/trophy.png',
         alternate: 'over 30 minutes!',
         trophyName: 'over 30 minutes!',
         locked: false
-    },           
-                       
+    },
+
         {
-        sauce: '../img/trophyImages/trophy.png',
+        source: '../img/trophyImages/trophy.png',
         alternate: 'more than 5 trophies!',
         trophyName: 'more than 5 trophies!',
         locked: false
-    },  
+    },
             {
-        sauce: '../img/trophyImages/trophy.png',
+        source: '../img/trophyImages/trophy.png',
         alternate: 'over 60 minutes!',
         trophyName: 'over 60 minutes!',
         locked: false
-    },              
-                       
+    },
+
         {
-        sauce: '../img/trophyImages/trophy.png',
+        source: '../img/trophyImages/trophy.png',
         alternate: 'nifty fifty!',
         trophyName: 'nifty fifty!',
         locked: false
     },  {
-        sauce: '../img/trophyImages/trophy.png',
+        source: '../img/trophyImages/trophy.png',
         alternate: 'over 100 pages!',
         trophyName: 'over 100 pages!',
         locked: false
     },  {
-        sauce: '../img/trophyImages/trophy.png',
+        source: '../img/trophyImages/trophy.png',
         alternate: 'took a book on a trip!',
         trophyName: 'took a book on a trip!',
         locked: false
-    }, 
+    },
         {
-        sauce: '../img/trophyImages/100Trophy.png',
+        source: '../img/trophyImages/100Trophy.png',
         alternate: 'read 100 books!',
         trophyName: 'read 100 books!',
         locked: false
-    }, 
-                       
+    },
+
 ];
 });
-    
