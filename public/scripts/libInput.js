6b5007cc754de805060388140f87661caf51de55
RedEis91 <imitationkrabs91@gmail.com>
2017-03-21 13:57:23 -0400

    clear form on submit

--- public/scripts/libInput.js
+++ public/scripts/libInput.js
@@ -7,41 +7,47 @@ app.controller('libInput', function($scope, dbFactory, readingFactory, studentFa
 
   //taking information from the form and when submit button is clicked it increments the book by one
   $scope.updateBooks = function(student) {
+     
       console.log(student);
     readingFactory.exportTo(student);
     updatedStudent.booksread++;
 
     if (student.time === "30 minutes" || "One Hour" || "More than an hour") {
         updatedStudent.thirtymin = true;
-        console.log("I ran for no reason");
+        console.log("thirty min is now true");
     }
        if (student.time ===  "One Hour" || "More than an hour" && student.time !== "30 minutes") {
         updatedStudent.sixtymin = true;
            console.log(student.time);
-           console.log("I also ran for no reason");
+           console.log("sixty min ran");
     }
 
       if (student.place !== "At Home" || "At School" ) {
           updatedStudent.trip = true;
-          console.log("I'm a big dumb idiot");
+          console.log("I took a trip");
       }
 
       if (student.pagesread === "Somewhere between 50 - 100 pages") {
           updatedStudent.overfifty = true;
-          console.log("Jerk code in effect");
+          console.log("fifty plus ran");
       }
        if (student.pagesread === "Somewhere between 100 - 200 pages") {
           updatedStudent.overfifty = true;
           updatedStudent.overonehundred = true;
-           console.log("I'm running for some reason");
+           console.log("one hundred plus ran");
       }
       if (student.pagesread === "200 or more pages") {
           updatedStudent.overfifty = true;
           updatedStudent.overonehundred = true;
           updatedStudent.overtwohundred = true;
-          console.log("I'm a crappy piece of code and I  just plain hate people");
+          console.log("two hundred ran");
       }
-
+     var clearField = document.getElementById("bookTitleField");
+      clearField.value = '';
+      console.log("Book title field cleared!");
+      
+      
+//myValue determines whether or not madLib output displays. true = yes false=no
       $scope.myValue = false;
       $scope.showAlert = function () {
         $scope.myValue = true;
