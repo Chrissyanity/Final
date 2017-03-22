7ad6e64dd676a61888841779f6e3ac9af94d8c6a
theadammason <adam.mason86@gmail.com>
2017-03-22 11:05:42 -0400

    styling tables

--- public/scripts/dbFactory.js
+++ public/scripts/dbFactory.js
@@ -20,14 +20,6 @@ app.factory('dbFactory', function($http) {
             method: 'GET',
             url: '/get-student'
         }).then(function success(response) {
-<<<<<<< HEAD
-<<<<<<< d5c194504fa9a8de20a675624710ab80c641714a
-=======
-
->>>>>>> changed intro list
-=======
-
->>>>>>> 0cd8bcca669bfba0ab568ddb66848ddf85a7f9c6
             student = response.data;
         });
         return promise;
@@ -35,13 +27,6 @@ app.factory('dbFactory', function($http) {
 
     //this function is for adding new students
     function addStudent(newStudent) {
-<<<<<<< HEAD
-<<<<<<< d5c194504fa9a8de20a675624710ab80c641714a
-
-=======
-        console.log('add student has been called');
-        console.log(newStudent);
->>>>>>> changed intro list
         var newStudent = {
             studentname: newStudent,
             booksread: 0
@@ -51,10 +36,6 @@ app.factory('dbFactory', function($http) {
             url: '/add-student',
             data: newStudent
         }).then(function success(response) {
-<<<<<<< d5c194504fa9a8de20a675624710ab80c641714a
-=======
-
->>>>>>> changed intro list
             student = response.data;
         });
         return promise;
@@ -62,10 +43,6 @@ app.factory('dbFactory', function($http) {
 
     //this function updates the number of books by id
     function updateBooksRead(updatedStudent) {
-
-=======
-        console.log('add student has been called');
-        console.log(newStudent);
         var newStudent = {
             studentname: newStudent,
             booksread: 0
@@ -83,23 +60,11 @@ app.factory('dbFactory', function($http) {
 
     //this function updates the number of books by id
     function updateBooksRead(updatedStudent) {
-
->>>>>>> 0cd8bcca669bfba0ab568ddb66848ddf85a7f9c6
         var promise = $http({
             method: 'PUT',
             url: '/update-books-read/' + updatedStudent.id,
             data: updatedStudent
         }).then(function success(response) {
-<<<<<<< HEAD
-<<<<<<< d5c194504fa9a8de20a675624710ab80c641714a
-=======
-            console.log("The student has been updated");
-            console.log(updatedStudent);
->>>>>>> changed intro list
-=======
-            console.log("The student has been updated");
-            console.log(updatedStudent);
->>>>>>> 0cd8bcca669bfba0ab568ddb66848ddf85a7f9c6
             student = response.data;
         });
         return promise;
