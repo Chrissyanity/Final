20df09ca7a7726ad4968395834de0acdf8e89c22
RedEis91 <imitationkrabs91@gmail.com>
2017-03-22 13:48:55 -0400

    deleted extra files, cleaned up libinput.js

--- public/scripts/libInput.js
+++ public/scripts/libInput.js
@@ -7,45 +7,36 @@ app.controller('libInput', function($scope, $route, dbFactory, readingFactory, s
  //taking information from the form and when submit button is clicked it increments the book by one
  $scope.updateBooks = function(student) {
 
-     console.log(student);
    readingFactory.exportTo(student);
    updatedStudent.booksread++;
 
    if (student.time === "30 minutes" || "One Hour" || "More than an hour") {
        updatedStudent.thirtymin = true;
-       console.log("thirty min is now true");
    }
       if (student.time ===  "One Hour" || "More than an hour" && student.time !== "30 minutes") {
        updatedStudent.sixtymin = true;
-          console.log(student.time);
-          console.log("sixty min ran");
    }
 
      if (student.place !== "At Home" || "At School" ) {
          updatedStudent.trip = true;
-         console.log("I took a trip");
      }
 
      if (student.pagesread === "Somewhere between 50 - 100 pages") {
          updatedStudent.overfifty = true;
-         console.log("fifty plus ran");
      }
       if (student.pagesread === "Somewhere between 100 - 200 pages") {
          updatedStudent.overfifty = true;
          updatedStudent.overonehundred = true;
-          console.log("one hundred plus ran");
      }
      if (student.pagesread === "200 or more pages") {
          updatedStudent.overfifty = true;
          updatedStudent.overonehundred = true;
          updatedStudent.overtwohundred = true;
-         console.log("two hundred ran");
      }
 
      //clears the bookTitleField of modal on 'submit'
     var clearField = document.getElementById("bookTitleField");
      clearField.value = '';
-     console.log("Book title field cleared!");
 
 
 //myValue determines whether or not madLib output displays. true = yes false=no
